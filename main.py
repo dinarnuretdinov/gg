6cbde0a1f128a0e2c1a4d6cb600c7a36d93358cf
dinarnuretdinov <113174592+dinarnuretdinov@users.noreply.github.com>
2025-09-19 12:47:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ st.title("Распознавание изображений с помощью Hu
 uploaded_file = st.file_uploader("Загрузите изображение", type = ["jpg", "jpeg", "png"])
 #Проверка, загружено ли изображение
 if uploaded_file is not None:
-# Открываем изображение с помощью PIL и выводим на экран
-image = Image.open(uploaded_file)
-st.image(image, caption="Загруженное изображение", use_container_width = True)
-# Инициализация модели для классификации изображений
-classifier = pipeline("image-classification", model="google/vit-base-patch16-224")
-# Распознавание изображения и запись результатов
-results = classifier(image)
-st.write("**Результаты распознавания**")
+  # Открываем изображение с помощью PIL и выводим на экран
+  image = Image.open(uploaded_file)
+  st.image(image, caption="Загруженное изображение", use_container_width = True)
+  # Инициализация модели для классификации изображений
+  classifier = pipeline("image-classification", model="google/vit-base-patch16-224")
+  # Распознавание изображения и запись результатов
+  results = classifier(image)
+  st.write("**Результаты распознавания**")
 for result in results:
-st.write(f"{result['label']}: {result['score']:.2f}")
+  st.write(f"{result['label']}: {result['score']:.2f}")
